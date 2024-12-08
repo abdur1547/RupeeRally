1ab8d9c5b1c8a69f7ba0fab9b2221cc8add66ee3
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-12-08 20:59:52 +0500

    toolbar component is rendered in app.

--- web/src/app/shared/components/toolbar/toolbar.component.ts
+++ web/src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { MaterialModule } from 'src/app/material.module';
 
 @Component({
   selector: 'toolbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgScrollbarModule, TablerIconsModule, MaterialModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  @Input() showToggle = true;
+  @Input() toggleChecked = false;
+  @Output() toggleMobileNav = new EventEmitter<void>();
+  @Output() toggleMobileFilterNav = new EventEmitter<void>();
+  @Output() toggleCollapsed = new EventEmitter<void>();
+}
