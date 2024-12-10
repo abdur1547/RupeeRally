38b0af94e8576722e78e69a47379bf6e1fd949d0
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-12-10 22:57:46 +0500

    moved routes to separeate routes array based on layout.

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,47 +2,51 @@ import { Routes } from '@angular/router';
 import { BlankLayout } from './layouts/blank/blank.component';
 import { DefaultLayout } from './layouts/default/full.component';
 
+const defaultLayoutRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/pages.routes').then((m) => m.PagesRoutes),
+  },
+  {
+    path: 'ui-components',
+    loadChildren: () =>
+      import('./pages/ui-components/ui-components.routes').then(
+        (m) => m.UiComponentsRoutes
+      ),
+  },
+  {
+    path: 'extra',
+    loadChildren: () =>
+      import('./pages/extra/extra.routes').then((m) => m.ExtraRoutes),
+  },
+];
+
+const blankLayoutRoutes: Routes = [
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('./pages/authentication/authentication.routes').then(
+        (m) => m.AuthenticationRoutes
+      ),
+  },
+];
+
 export const routes: Routes = [
   {
     path: '',
     component: DefaultLayout,
-    children: [
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full',
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./pages/pages.routes').then((m) => m.PagesRoutes),
-      },
-      {
-        path: 'ui-components',
-        loadChildren: () =>
-          import('./pages/ui-components/ui-components.routes').then(
-            (m) => m.UiComponentsRoutes
-          ),
-      },
-      {
-        path: 'extra',
-        loadChildren: () =>
-          import('./pages/extra/extra.routes').then((m) => m.ExtraRoutes),
-      },
-    ],
+    children: [...defaultLayoutRoutes],
   },
   {
     path: '',
     component: BlankLayout,
-    children: [
-      {
-        path: 'authentication',
-        loadChildren: () =>
-          import('./pages/authentication/authentication.routes').then(
-            (m) => m.AuthenticationRoutes
-          ),
-      },
-    ],
+    children: [...blankLayoutRoutes],
   },
   {
     path: '**',
