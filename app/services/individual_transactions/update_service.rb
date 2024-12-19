a7b3f3b3e8c46a03d8d771daae7301e23ff09f8d
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-12-19 11:26:34 +0000

    updated transaction#update to use new helper methods.

--- app/services/individual_transactions/update_service.rb
+++ app/services/individual_transactions/update_service.rb
@@ -2,15 +2,16 @@
 
 module IndividualTransactions
   class UpdateService < ::BaseService
+    include Helpers::TransactionHelpers
+
     def call(params)
-      process_params(params)
+      validate_params(params)
+      process_params
       set_previous_data
 
       ActiveRecord::Base.transaction do
-        revert_previous_account_change
-        revert_previous_category_change
-        add_updated_amount_to_account
-        add_updated_amount_to_category
+        revert_amount_changes(previous_amount, previous_account, previous_category, previous_direction)
+        apply_amount_changes(amount_cents, account, category, direction)
         update_parent_transaction
       end
 
@@ -20,20 +21,25 @@ module IndividualTransactions
     private
 
     attr_reader :params,
+                :parent_transaction,
                 :description,
                 :amount_cents,
                 :direction,
                 :account,
                 :category,
-                :parent_transaction,
                 :previous_amount,
                 :previous_account,
-                :previous_category
+                :previous_category,
+                :previous_direction
 
-    def process_params(params)
-      @params = params
+    def validate_params(params)
+      @params = params.symbolize_keys
       @parent_transaction = params[:parent_transaction]
-      @description = params[:description]
+      raise ArgumentError, 'Parent transaction is required' unless parent_transaction
+    end
+
+    def process_params
+      @description = params[:description] || parent_transaction.description
       @amount_cents = params[:amount_cents] || parent_transaction.amount_cents
       @direction = params[:direction] || parent_transaction.direction
       @account = params[:account] || parent_transaction.account
@@ -44,34 +50,7 @@ module IndividualTransactions
       @previous_amount = parent_transaction.amount_cents
       @previous_account = parent_transaction.account
       @previous_category = parent_transaction.category
-    end
-
-    def revert_previous_account_change
-      if parent_transaction.expense?
-        previous_account.record_expense(-previous_amount)
-      else
-        previous_account.record_income(-previous_amount)
-      end
-      previous_account.save!
-    end
-
-    def revert_previous_category_change
-      previous_category.update_balance(-previous_amount)
-      previous_category.save!
-    end
-
-    def add_updated_amount_to_account
-      if direction == 'expense'
-        account.reload.record_expense(amount_cents)
-      else
-        account.reload.record_income(amount_cents)
-      end
-      account.save!
-    end
-
-    def add_updated_amount_to_category
-      category.reload.update_balance(amount_cents)
-      category.save!
+      @previous_direction = parent_transaction.direction
     end
 
     def update_parent_transaction
