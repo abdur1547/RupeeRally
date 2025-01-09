8792fa4c0870ab70bcd72a5cb2b69256b7617e62
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-01-09 08:38:39 +0000

    rails g annotate:install

--- app/models/blacklisted_token.rb
+++ app/models/blacklisted_token.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: blacklisted_tokens
+#
+#  id         :integer          not null, primary key
+#  jti        :string
+#  user_id    :integer          not null
+#  exp        :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_blacklisted_tokens_on_jti      (jti) UNIQUE
+#  index_blacklisted_tokens_on_user_id  (user_id)
+#
+
 # frozen_string_literal: true
 
 class BlacklistedToken < ApplicationRecord
