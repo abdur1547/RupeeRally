6db4035a9a93125199b6afd861cb07611677b520
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-01-09 08:40:06 +0000

    auto annotate models.

--- app/models/refresh_token.rb
+++ app/models/refresh_token.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: refresh_tokens
+#
+#  id            :integer          not null, primary key
+#  crypted_token :string
+#  user_id       :integer          not null
+#  exp           :datetime         not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_refresh_tokens_on_crypted_token  (crypted_token) UNIQUE
+#  index_refresh_tokens_on_user_id        (user_id)
+#
+
 # frozen_string_literal: true
 
 class RefreshToken < ApplicationRecord
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: user_groups
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  group_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_user_groups_on_group_id  (group_id)
+#  index_user_groups_on_user_id   (user_id)
+#
+
 # frozen_string_literal: true
 
 class UserGroup < ApplicationRecord
