32576e7ed90ec60ebdf94d8cd91907663bd9058b
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-01-11 22:29:19 +0500

    if token is not present redirect to login

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SnackBarComponent } from './shared';
 import { TokenService } from './core';
 
@@ -10,14 +10,22 @@ import { TokenService } from './core';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit, OnDestroy {
+  private readonly router: Router = inject(Router);
   private readonly tokenService: TokenService = inject(TokenService);
   title = 'RupeeRally';
 
   ngOnInit() {
+    this.checkToken();
     this.tokenService.autoRefreshAccessToken();
   }
 
   ngOnDestroy() {
     this.tokenService.clearTimer();
+  }
+
+  checkToken() {
+    if (!this.tokenService.getAccessToken()) {
+      this.router.navigate(['authentication/login']);
+    }
   }
 }
