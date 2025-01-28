00c271dd5aaabe0b658bac047f3234d269a9ecc0
ehtishamalik <77209161+ehtishamalik@users.noreply.github.com>
2025-01-29 00:38:07 +0500

    Added basic login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section>
+      <div className="flex flex-row">
+        <div className="basis-[60%]">
+          <header>
+            <h1>Login to Rupee Rally</h1>
+          </header>
+        </div>
+        <div className="basis-[40%] bg-accent">
+          <div></div>
+          <div className="max-w-96 mx-auto text-background text-center">
+            <h2>Let's Continue Simplifying Your Expenses!</h2>
+            <p>Log in to stay in sync with your friends and finances.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
