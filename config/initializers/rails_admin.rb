d60302a81995f1a5c1edd02cc967013dc6a83a83
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-01-28 14:39:10 +0000

    only admin users can access /admin & /sidekiq route

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,7 +6,7 @@ RailsAdmin.config do |config|
   ## == Devise ==
   config.authenticate_with do
     warden.authenticate! scope: :user
-    # redirect_to main_app.root_path, alert: 'You are not authorized to access this page!' unless current_user.admin?
+    redirect_to main_app.root_path, alert: 'You are not authorized to access this page!' unless current_user.admin?
   end
   config.current_user_method(&:current_user)
 
