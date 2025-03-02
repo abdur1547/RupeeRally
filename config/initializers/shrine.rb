4638f33258f94ca65145246a46ba42324e8e28ce
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-03-02 09:41:49 +0000

    use different shrine settings for prod end

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,10 +3,14 @@
 require 'shrine'
 require 'shrine/storage/file_system'
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads')
-}
+if Rails.env.production?
+  # setup prod service for shrine
+else
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads')
+  }
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
