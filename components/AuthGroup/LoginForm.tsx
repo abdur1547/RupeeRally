ed83be001c5a3abcfbafc1cd15165f0cd7408a5c
ehtishamalik <77209161+ehtishamalik@users.noreply.github.com>
2025-03-03 13:55:38 +0500

    updated login form

--- components/AuthGroup/LoginForm.tsx
+++ components/AuthGroup/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import IconInput from "./IconInput";
 import InputPassword from "./InputPassword";
 import { Button } from "../ui/button";
@@ -8,30 +8,33 @@ import Link from "next/link";
 import { Mail } from "lucide-react";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState<string>("");
-  const [emailError, setEmailError] = useState<string>();
-
-  const [password, setPassword] = useState<string>("");
-  const [passwordError, setPasswordError] = useState<string>();
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^.{8,}$/;
 
-  const onSubmit = async () => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email")?.toString() || "";
+    const password = formData.get("password")?.toString() || "";
+
     let valid = true;
 
-    if (!emailRegex.test(email)) {
+    if (!email || !emailRegex.test(email)) {
       setEmailError("Please enter a valid email address.");
       valid = false;
     } else {
-      setEmailError("");
+      setEmailError(null);
     }
 
-    if (!passwordRegex.test(password)) {
+    if (!password || !passwordRegex.test(password as string)) {
       setPasswordError("Password must be at least 8 characters.");
       valid = false;
     } else {
-      setPasswordError("");
+      setPasswordError(null);
     }
 
     if (valid) {
@@ -41,26 +44,20 @@ const LoginForm = () => {
 
   return (
     <div className="space-y-4">
-      <form className="space-y-5">
+      <form onSubmit={handleSubmit} className="space-y-5">
         <div>
-          <IconInput
-            Icon={Mail}
-            placeholder="Email Address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-          />
+          <IconInput Icon={Mail} placeholder="Email Address" type="email" name="email" />
           {emailError && <p className="text-destructive">{emailError}</p>}
         </div>
         <div>
-          <InputPassword value={password} onChange={(e) => setPassword(e.target.value)} />
+          <InputPassword name="password" />
           {passwordError && <p className="text-destructive">{passwordError}</p>}
         </div>
         <div className="flex flex-col space-y-7">
           <Button variant="link" type="button" className="ml-auto">
             Forget Password?
           </Button>
-          <Button onClick={onSubmit}>Login</Button>
+          <Button>Login</Button>
         </div>
       </form>
       <p className="text-center">
