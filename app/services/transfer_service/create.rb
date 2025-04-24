9f9d2fd6708f11e72cc6e611733cea966c0b592c
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-04-24 12:33:40 +0000

    updating services

--- app/services/transfer_service/create.rb
+++ app/services/transfer_service/create.rb
@@ -1,21 +1,16 @@
 # frozen_string_literal: true
 
-module TransferTransactions
-  class CreateService < ::BaseService
+module TransferService
+  class Create < ::BaseService
     include Helpers::TransactionHelpers
 
     def call(params)
       process_params(params)
 
-      ActiveRecord::Base.transaction do
-        create_parent_transaction
-        create_from_account_transaction
-        create_to_account_transaction
-        adjust_account_balance(from_account, amount_cents, :expense)
-        adjust_account_balance(to_account, amount_cents, :income)
-      end
-
-      parent_transaction
+      ActiveRecord::Base.transaction { perform_transfer_operations }
+      { success: true, transfer: @transfer }
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved, ActiveRecord::StatementInvalid => e
+      { success: false, errors: e.message }
     end
 
     private
@@ -25,7 +20,7 @@ module TransferTransactions
                 :to_account,
                 :description,
                 :amount_cents,
-                :parent_transaction
+                :transfer
 
     def process_params(params)
       @current_user = params[:current_user]
@@ -35,14 +30,20 @@ module TransferTransactions
       @amount_cents = params[:amount_cents]
     end
 
-    def create_parent_transaction
-      parent_trn_desc = "Transferred from #{from_account.name} to #{to_account.name} account"
-      @parent_transaction = Transaction.create!(user: current_user,
-                                                description: parent_trn_desc,
-                                                amount_cents:,
-                                                divided_by: :by_none,
-                                                paid_by: current_user,
-                                                transaction_type: :transfer)
+    def perform_transfer_operations
+      create_transfer
+      create_from_account_transaction
+      create_to_account_transaction
+      adjust_account_balance(from_account, amount_cents, :expense)
+      adjust_account_balance(to_account, amount_cents, :income)
+    end
+
+    def create_transfer
+      @transfer = Transfer.create!(user: current_user,
+                                   description:,
+                                   amount_cents:,
+                                   from_account:,
+                                   to_account:)
     end
 
     def create_from_account_transaction
@@ -50,11 +51,9 @@ module TransferTransactions
       Transaction.create!(user: current_user,
                           description: from_acc_desc,
                           direction: :expense,
-                          user_share: 100,
                           amount_cents:,
                           account: from_account,
-                          parent_transaction:,
-                          paid_by: current_user)
+                          transfer:)
     end
 
     def create_to_account_transaction
@@ -62,11 +61,9 @@ module TransferTransactions
       Transaction.create!(user: current_user,
                           description: to_acc_desc,
                           direction: :income,
-                          user_share: 100,
                           amount_cents:,
                           account: to_account,
-                          parent_transaction:,
-                          paid_by: current_user)
+                          transfer:)
     end
   end
 end
---
+++ spec/factories/transfers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :transfer do
+    amount_cents { 1 }
+    description { "MyText" }
+  end
+end
