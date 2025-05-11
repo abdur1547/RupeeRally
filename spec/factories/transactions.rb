1d20d1a0aa57fb271b00943895f1584db4a15fdd
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-05-11 10:34:18 +0000

    factories are updated.

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,16 +2,11 @@ FactoryBot.define do
   factory :transaction do
     description { Faker::Lorem.sentence }
     amount_cents { Faker::Number.number(digits: 5) }
-    divided_by { :by_none }
-    transaction_type { :individual }
     direction { :income }
     status { :pending }
-    user_share { 100 }
     selected_date { Date.today.strftime(Constants::API_DATE_FROMAT) }
     selected_time { Time.now.strftime(Constants::API_TIME_FROMAT) }
-    user { create(:user) }
-    paid_by { user }
-    parent_transaction_id { nil }
-    child_transactions {[]}
+    user { association(:user) }
+    account { association(:account, user:) }
   end
 end
--- spec/factories/transfers.rb
+++ spec/factories/transfers.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
   factory :transfer do
-    amount_cents { 1 }
-    description { "MyText" }
+    amount_cents { Faker::Number.number(digits: 5) }
+    description { Faker::Lorem.sentence }
+    user { association(:user) }
+    from_account { association(:account, user:) }
+    to_account { association(:account, user:) }
+    transactions do
+      [create(:transaction, description:, amount_cents:, account: from_account, user:),
+       create(:transaction, description:, amount_cents:, account: to_account, user:)]
+    end
   end
 end
